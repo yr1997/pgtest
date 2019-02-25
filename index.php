512182a87da57bd857a51b667e67df41d0fae69d
yr1997 <45040956+yr1997@users.noreply.github.com>
2019-02-25 17:49:10 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,31 @@
 <?php
-// 変数の初期化
-$res = null;
-$pdo_conn = null;
-$sql = null;
-try {
-	// データベースと接続
-	$pdo_conn = new PDO( 'pgsql:host=ec2-23-21-165-188.compute-1.amazonaws.com; dbname=d81tl9ncnqmhr0;', 'gyuzauumvdcvqw', '0f44482e44311c9eaf16bb24ab22a891a521054e81c4215ef53a2592e2e3de1b' );	
-	// テーブル作成のためのSQL
-	$sql = "CREATE TABLE menu (
-	id int PRIMARY KEY,
-	name varchar(50),
-	price int,
-	modify_datetime date,
-	create_datetime date
-)";
-	// SQLクエリ実行
-	$res = $pdo_conn->query( $sql);
-	var_dump($res);
-} catch(PDOException $e) {
-	var_dump($e->getMessage());
-}
-// データベースの接続を切断
-$pdo_conn = null;
 
+// 変数の初期化
+$sql = null;
+$res = null;
+$dbh = null;
+
+try {
+	// DBへ接続
+	$dbh = new PDO( 'pgsql:host=ec2-23-21-165-188.compute-1.amazonaws.com; dbname=d81tl9ncnqmhr0;', 'gyuzauumvdcvqw', '0f44482e44311c9eaf16bb24ab22a891a521054e81c4215ef53a2592e2e3de1b' );
+	// SQL作成
+	$sql = "SELECT * FROM user_list";
+
+	// SQL実行
+	$res = $dbh->query($sql);
+
+	// 取得したデータを出力
+	foreach( $res as $value ) {
+		echo "$value[name]<br>";
+	}
+
+} catch(PDOException $e) {
+	echo $e->getMessage();
+	die();
+}
+
+// 接続を閉じる
+$dbh = null;
 
 
 

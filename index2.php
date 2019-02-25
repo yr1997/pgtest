ae424dd642db72cf404d4f9bc8a584b7fb8b689b
yr1997 <45040956+yr1997@users.noreply.github.com>
2019-02-25 17:24:17 +0900

    Update index2.php

--- index2.php
+++ index2.php
@@ -11,7 +11,23 @@ $date = date("Y-m-d");
 
 try {
 	// データベースと接続
-	$pdo_conn = new PDO( 'pgsql:host=localhost; dbname=test;', 'testuser', 'testtest' );	
+	$pdo_conn = new PDO( 'pgsql:host=ec2-23-21-165-188.compute-1.amazonaws.com; dbname=d81tl9ncnqmhr0;', 'gyuzauumvdcvqw', '0f44482e44311c9eaf16bb24ab22a891a521054e81c4215ef53a2592e2e3de1b' );	
+	// テーブル作成のためのSQL
+	$sql = "CREATE TABLE menu (
+	id int PRIMARY KEY,
+	name varchar(50),
+	price int,
+	modify_datetime date,
+	create_datetime date
+)";
+	// SQLクエリ実行
+	$res = $pdo_conn->query( $sql);
+	var_dump($res);
+} catch(PDOException $e) {
+	var_dump($e->getMessage());
+}
+// データベースの接続を切断
+$pdo_conn = null;
 
 	// データを登録するためのSQLを作成
 	$sql = "INSERT INTO menu (
